11d3d5a3bc85d602e3233809b51741346ca99b2d
Hong Vouchkim <vuochkimhong99@gmail.com>
2022-04-26 16:03:16 +0700

    commit gradeint

--- AllUIElement/UIGradientColor/GradientColorViewController.swift
+++ AllUIElement/UIGradientColor/GradientColorViewController.swift
@@ -9,15 +9,8 @@ import UIKit
 
 class GradientCollorViewController: UIViewController {
 	
-	@IBOutlet weak var myView: UIView!
 	override func viewDidLoad() {
         super.viewDidLoad()
-		let gradient = CAGradientLayer()
-		gradient.frame = view.bounds
-		gradient.colors = [UIColor.red.cgColor, UIColor.blue.cgColor]
-		gradient.startPoint = CGPoint(x: 0.0, y: 0.5)
-		gradient.endPoint = CGPoint(x: 1.0, y: 0.5)
-		myView.layer.addSublayer(gradient)
     }
 
 }
